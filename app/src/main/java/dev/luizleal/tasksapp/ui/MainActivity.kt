29854d1ee4d58e2c0775315a584d5753f6de3847
luizleal <luizandreleal100@gmail.com>
2024-06-20 19:11:04 -0300

    feat: new task alert dialog with editText

--- app/src/main/java/dev/luizleal/tasksapp/ui/MainActivity.kt
+++ app/src/main/java/dev/luizleal/tasksapp/ui/MainActivity.kt
@@ -1,21 +1,50 @@
 package dev.luizleal.tasksapp.ui
 
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dev.luizleal.tasksapp.R
+import dev.luizleal.tasksapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.apply {
+            fabNewTask.setOnClickListener(setupNewTaskDialog())
+        }
+    }
+
+    private fun setupNewTaskDialog(): View.OnClickListener? {
+        return View.OnClickListener {
+            val dialogView = layoutInflater.inflate(R.layout.new_note_dialog, null)
+            val tasksTitleEditText = dialogView.findViewById<EditText>(R.id.edit_task_title)
+
+            MaterialAlertDialogBuilder(this@MainActivity)
+                .setView(dialogView)
+                .setPositiveButton(getString(R.string.new_task_positive_button)) { _, _ ->
+                    val inputText = tasksTitleEditText.text.toString()
+                }
+                .setNegativeButton(getString(R.string.new_task_negative_button)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create().show()
         }
     }
 }
