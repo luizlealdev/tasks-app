992c3b15a8a6786b1e450a7f59db4ee54173ab80
luizleal <luizandreleal100@gmail.com>
2024-07-07 09:46:19 -0300

    fix: wrong class for textLayout

--- app/src/main/java/dev/luizleal/tasksapp/view/MainActivity.kt
+++ app/src/main/java/dev/luizleal/tasksapp/view/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 import dev.luizleal.tasksapp.R
 import dev.luizleal.tasksapp.databinding.ActivityMainBinding
 
@@ -33,7 +34,7 @@ class MainActivity : AppCompatActivity() {
     private fun setupNewTaskDialog(): View.OnClickListener? {
         return View.OnClickListener {
             val dialogView = layoutInflater.inflate(R.layout.new_note_dialog, null)
-            val tasksTitleEditText = dialogView.findViewById<EditText>(R.id.edit_task_title)
+            val tasksTitleEditText = dialogView.findViewById<TextInputEditText>(R.id.textfield_title)
 
             MaterialAlertDialogBuilder(this@MainActivity)
                 .setView(dialogView)
