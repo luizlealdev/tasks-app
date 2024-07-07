1612b102f70904b491642c9af61cfa949f128c76
luizleal <luizandreleal100@gmail.com>
2024-07-07 14:33:54 -0300

    fix: task model updated to a entity for db

--- app/src/main/java/dev/luizleal/tasksapp/model/Task.kt
+++ app/src/main/java/dev/luizleal/tasksapp/model/Task.kt
@@ -1,3 +1,12 @@
 package dev.luizleal.tasksapp.model
 
-data class Task(val taskTitle: String, val isChecked: Boolean)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_table")
+data class Task(
+    @PrimaryKey(autoGenerate = true) var id: Int? = null,
+    @ColumnInfo(name = "task_title") val taskTitle: String,
+    @ColumnInfo(name = "is_checked") val isChecked: Boolean = false
+)
